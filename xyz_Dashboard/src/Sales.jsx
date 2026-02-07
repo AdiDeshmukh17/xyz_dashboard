ccb8deda43aeef90135286598382e7d3e8ff144a
Aditya Deshmukh <aditya.r.deshmukh3117@gmail.com>
2026-02-08 00:53:11 +0530

    Updated 2 files

--- xyz_Dashboard/src/Sales.jsx
+++ xyz_Dashboard/src/Sales.jsx
@@ -1,16 +1,10 @@
 export default function Sales() {
   return (
-    <div className="dashboard-container">
-      <iframe
-        title="Sales Dashboard"
-        src="https://public.tableau.com/views/SalesDashbord_17704438883150/Dashboard1?:embed=y&:showVizHome=no"
-        style={{
-          width: "100%",
-          height: "800%",
-          border: "none"
-        }}
-        allowFullScreen
-      />
-    </div>
+    <iframe
+      className="tableau-frame"
+      title="Sales Dashboard"
+      src="https://public.tableau.com/views/SalesDashbord_17704438883150/Dashboard1?:embed=y&:showVizHome=no"
+      allowFullScreen
+    />
   );
 }
